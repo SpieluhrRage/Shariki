bd2600e8e0ec52ffbe0579de13bb5caae17f4e26
Тимур Платонов <gered372@gmail.com>
2023-11-28 10:16:25 +0300

    finished

--- Shariki/Source.cpp
+++ Shariki/Source.cpp
@@ -1,28 +1,45 @@
 #include<iostream>
 using namespace std;
-int generate(int t, int n, int* mas) {
+void generate(int t, int n, int* mas, int &ct) {
+	
+	bool fl = false;
+	
 	if (t == (n - 1)) {
-		for (int i = 0; i < n; ++i) {
+		/*for (int i = 0; i < n; ++i) {
 			cout << mas[i] << " ";
 			
 		}
-		cout << endl;
-		return *mas;
+		cout << endl;*/
+		for (int i = 0; i <= 8; i++) {
+			if (i == mas[i]) {
+				fl = true;
+			}
+		}
+		if (fl) {
+			ct++;
+			fl = false;
+		}
+			
+		
 	}
 	else {
+		
 		for (int j = t; j < n; j++) {
 			swap(mas[t], mas[j]);
 			t++;
-			generate(t, n, mas);
+			generate(t, n, mas, ct);
 			t--;
 			swap(mas[t], mas[j]);
 		}
 	}
+	
 	}
 
 int main() {
+	int ct = 0;
 	int mass[9] = { 1, 0, 2, 8, 3, 7, 4, 6, 5 };
-	for (int i = 0; i <= 8; i++) {
+	generate(0, 9, mass, ct);
+	cout << ct;
+	
 
-	}
 }
